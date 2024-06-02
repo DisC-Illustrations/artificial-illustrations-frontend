b2560fcda043c6101cfd5ad1de3dcdc0b8211ea9
sara-elenx <sara@fnego.de>
2024-06-02 12:38:19 +0200

    basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
 <div>
-    <h1 class="font-sans font-medium">Hi</h1>
-    <h1 class="font-sans font-regular">Hi</h1>
-    <h1 class="font-sans font-bold">Hi</h1>
+    <h1 class="font-sans font-medium">Hier kommt das Formular hin</h1>
 </div>
