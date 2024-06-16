a0d57c5b0e7e2e7094d6f8c471acbcbdc3705c1d
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-06-16 16:59:25 +0200

    adding loading animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,30 @@
 <script lang="ts">
-    import type { PageServerData } from "./$types";
-    import { type Prompt, PromptSchema } from "$lib/types";
-    import { superForm } from "sveltekit-superforms";
-    import { zod } from "sveltekit-superforms/adapters";
-    import { string } from "zod";
-    import { onMount } from "svelte";
+    import  "./style/loading.css";
+
+    import type {PageServerData} from "./$types";
+    import {type Prompt, PromptSchema} from "$lib/types";
+    import {superForm} from "sveltekit-superforms";
+    import {zod} from "sveltekit-superforms/adapters";
+    import {string} from "zod";
+    import {onMount} from "svelte";
     import PrimaryButton from "$lib/components/buttons/primary-button.svelte";
     import TextArea from "$lib/components/inputs/text-area.svelte";
 
 
     export let data: PageServerData;
 
+    // fixme: default state is false
+    $: loading = true;
     $: images = [];
 
     const formData: Prompt = data.prompt;
 
-    const { form, enhance } = superForm(formData, {
+    const {form, enhance} = superForm(formData, {
         SPA: true,
         validators: zod(PromptSchema),
         taintedMessage: false,
         invalidateAll: false,
-        onUpdated: async ({ form }) => {
+        onUpdated: async ({form}) => {
             if (form.valid) {
                 const response = await fetch("/api/", {
                     method: "POST",
@@ -38,7 +42,9 @@
 
 
     let inputText = '';
+    let imageElements = '';
     let responseText = '';
+    let finalPrompt = '';
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -66,12 +72,13 @@
 
 
 <form on:submit={handleSubmit}>
-    <TextArea 
-        bind:value={inputText} 
-        placeholder="Enter your prompt" 
-        label="Prompt"
-        rows={5}
-        required={true}
+    <TextArea
+            bind:value={inputText}
+            placeholder="Enter your prompt"
+            name="Text"
+            label="Prompt"
+            rows={5}
+            required={true}
     />
     <button type="submit">Submit</button>
 </form>
@@ -80,5 +87,28 @@
     <div>
         <h3>Response:</h3>
         <p>{responseText}</p>
+    </div>
+{/if}
+
+<!-- show placeholder, loading animation, or images -->
+{#if images.length > 0}
+    <div>
+        {#each images as image}
+            <img src={image} alt="Generated image based on {finalPrompt}" />
+        {/each}
+    </div>
+{/if}
+{#if loading}
+    <div class="loading-animation">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
     </div>
 {/if}
