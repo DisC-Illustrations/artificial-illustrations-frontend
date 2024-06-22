3abeb3fa7224c1623426bb4d9eef2c32ef1485e6
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-06-22 18:43:05 +0200

    adding first OpenAI system prompt (just a try)

--- src/routes/api/generate/+server.ts
+++ src/routes/api/generate/+server.ts
@@ -1,16 +1,22 @@
-import type { RequestHandler } from '@sveltejs/kit';
+import type {RequestHandler} from '@sveltejs/kit';
 import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const POST: RequestHandler = async ({ request }) => {
-    const { prompt } = await request.json();
+export const POST: RequestHandler = async ({request}) => {
+    const {prompt} = await request.json();
 
     try {
         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
             model: 'gpt-4',
-            messages: [{ role: 'user', content: prompt }],
+            messages: [
+                {
+                    role: 'system',
+                    content: 'Capture the essence of the following text in 2-3 fitting visual Elements and return them to be used in a Stable Diffusion model (e.g. a painting, rain, dark atmosphere).'
+                },
+                {role: 'user', content: prompt}
+            ],
             temperature: 1,
             max_tokens: 256,
             top_p: 1,
@@ -23,9 +29,9 @@ export const POST: RequestHandler = async ({ request }) => {
             }
         });
 
-        return new Response(JSON.stringify({ text: response.data.choices[0].message.content }), { status: 200 });
+        return new Response(JSON.stringify({text: response.data.choices[0].message.content}), {status: 200});
     } catch (error) {
         console.error('Error communicating with OpenAI API:', error);
-        return new Response(JSON.stringify({ error: 'Error communicating with OpenAI API' }), { status: 500 });
+        return new Response(JSON.stringify({error: 'Error communicating with OpenAI API'}), {status: 500});
     }
 };
